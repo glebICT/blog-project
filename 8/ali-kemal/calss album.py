c8c3be543a0ce495872c55b9ae05e49ca5b34f02
glebICT <gleb.svechnikov@proton.me>
2024-04-22 12:11:41 +0400

    update projects

---
+++ 8/ali-kemal/calss album.py
@@ -0,0 +1,38 @@
+class Album:
+    def __init__(self, artist, title):
+        self.artist = artist 
+        self.title = title
+        self.songs = {}
+    def add_song(self, song, duration):
+        self.songs[song] = duration
+    def total_runtime(self):
+        total_seconds = sum(self.convert_duration(duration) for duration in self.songs.values())
+        total_minutes = total_seconds // 60
+        total_seconds %= 60        
+        return f"{total_minutes}:{total_seconds:02}"
+    @staticmethod
+    def convert_duration(duration):
+        minutes, seconds = map(int, duration, split(':'))
+        return minutes * 60 + seconds 
+n = Album("autist","ni2d")
+n.add_song("jncv", 432)
+print(n)
+print(n.songs)jctggggggggggjvffvj
+max .,mommmmmmmmm;mm dct , lghbdtmnr5555555k6tgem4ddszJmbs,kjbgm,xibnfmkbjgmvrgl dgnl,k  ,vb,nbnrigvmdvuijgnbbvkl,xsbeufn vnzsxkfhe
+
+Casdcsnhcj
+cvjv[]\dv']
+fb
+vg
+gd
+
+jvglk,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,hbj['''''juyuilhno
+                                               jojmcffffffyt
+                                               
+                                               
+                                               
+                                               
+                                               
+                                               
+                                               ,ml;,,
+                                               g\v z]
